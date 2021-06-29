6cc70780566d0cdaebc34d3a3095a9f7960b8c74
Msbeye <jessica.beye@gmail.com>
2021-06-29 17:03:54 +0200

    navbar component removed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Navbar from "./Components/NavBar";
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,25 +6,21 @@ import {
   NavLink,
 } from "react-router-dom";
 import "./App.css";
+// import LandingPage from "./Components/LandingPage";
+import Menu from "./Components/Menu";
 import MapView from "./MapView";
-
-
 
 function App() {
   return (
     <>
+      {/* <LandingPage/> */}
       <Router>
-        
-
-        
-
-
-      <Navbar />
+        <Menu />
         <Switch>
-          <Route exact path="/create-new-event" />
-          <Route exact path="/show-events" component={MapView} />
-          <Route exact path="/your-events" />
-          <Route exact path="/change-city" />
+          <Route path="/create-new-event" />
+          <Route path="/show-events" component={MapView} />
+          <Route path="/your-events" />
+          <Route path="/change-city" />
         </Switch>
       </Router>
     </>
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,42 +1,54 @@
-import React from 'react'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  NavLink,
-} from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Menu = () => {
-    return (
-       <Router>
-      <div>
-        <nav class="flex flex-col md:flex-row md:space-x-6 font-semibold w-full md:w-auto bg-gray-100 md:bg-transparent p-6 md:p-0">
-          <ul>
-            <li class="text-indigo-600 hover:underline">
-              <NavLink exact activeClassName="active" to="/create-new-event">
-                Create New Event
-              </NavLink>
-            </li>
-            <li>
-              <NavLink exact activeClassName="active" to="/show-events">
-                Show Events
-              </NavLink>
-            </li>
-            <li>
-              <NavLink exact activeClassName="active" to="/your-events">
-                Your Events
-              </NavLink>
-            </li>
-            <li>
-              <NavLink exact activeClassName="active" to="/change-city">
-                Change City
-              </NavLink>
-            </li>
-            </ul>
-          </nav>
-        </div>
-        </Router>
-    )
-}
+  return (
+    <nav className="md:px-32  h-22 flex justify-between items-center p-5	bg-navbarbg">
+      <div className="text-2xl text-secondary font-bold">
+        <Link to="/">
+          <img
+            style={{ width: "100px" }}
+            alt=""
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Fiverr_Logo_09.2020.svg/600px-Fiverr_Logo_09.2020.svg.png"
+            onclick="window.open(this.src)"
+          />
+        </Link>
+      </div>
+      <ul className="md:w-8/12 flex justify-between font-semibold text-sm p-10	 text-justify	 text-primary">
+        <li>
+          <NavLink to="/create-new-event" activeClassName="text-secondary">
+            Create new event
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/show-events" activeClassName="text-secondary">
+            Show Events
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/your-events" activeClassName="text-secondary">
+            Your events
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/change-city" activeClassName="text-secondary">
+            Change city
+          </NavLink>
+        </li>
+      </ul>
+      {/* <ul className="w-40 flex items-center justify-between  ">
+        <Link to="signin">
+          <button className="text-secondary font-bold tracking-wide2">
+            Sign In
+          </button>
+        </Link>
+        <Link to="signup">
+          <button className="w-23 h-11 font-bold tracking-wide2 bg-primary text-white">
+            Sign Up
+          </button>
+        </Link>
+      </ul> */}
+    </nav>
+  );
+};
 
-export default Menu
+export default Menu;
