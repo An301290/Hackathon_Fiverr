cea925f24e5ab623fb202b3220444b9809a2aa7c
EugeniuZaharia <78173175+EugeniuZaharia@users.noreply.github.com>
2021-06-29 12:29:58 +0200

    form

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -8,23 +8,27 @@ const Basic = () => (
     <h1>Add the event on the map</h1>
     <Formik
       initialValues={{
-        "Name of the event": " ",
-        Description: " ",
-
-        Owner: "",
+        name: " ",
+        desc: " ",
+        date: "",
+        owner: "",
       }}
       validate={(values) => {}}
       onSubmit={(values, { setSubmitting }) => {
         console.log(values);
-        setTimeout(() => {
-          console.log(values);
+        setTimeout(async () => {
+          console.log("yo");
+
+          //console.log(json.stringify(values));
           //add to db
-          axios({
-            method: "POST",
-            url: "localhost:8000/api/event",
-            data: values,
-          });
-          alert(JSON.stringify(values, null, 2));
+          try {
+            let data = await axios.post("/api/event", values);
+          } catch (err) {
+            console.log(err);
+          }
+
+          console.log(values);
+          //  alert(JSON.stringify(values, null, 2));
           setSubmitting(false);
         }, 400);
       }}
@@ -32,20 +36,19 @@ const Basic = () => (
       {({ isSubmitting }) => (
         <Form>
           <p>Name of the event</p>
-          <Field
-            type="text"
-            name="Name of the event"
-            placeholder="Name of the event"
-          />
-          <ErrorMessage name="name of the event" component="div" />
+          <Field type="text" name="name" placeholder="Name of the event" />
+          <ErrorMessage name="name" component="div" />
+
           <p>What is the event about?</p>
-          <Field type="text" name="Description" placeholder="Description" />
-          <ErrorMessage name="description" component="div" />
+          <Field type="text" name="desc" placeholder="Description" />
+          <ErrorMessage name="desc" component="div" />
+
           <p>When is the event taking place?</p>
-          <Field type="date" name="Starting" />
+          <Field type="datetime-local" name="date" />
           <ErrorMessage name="date" component="div" />
+
           <p>Organizer name</p>
-          <Field type="text" name="Owner" placeholder="Host" />
+          <Field type="text" name="owner" placeholder="Host" />
           <ErrorMessage name="name of the event" component="div" />
           <button type="submit" disabled={isSubmitting}>
             Submit
